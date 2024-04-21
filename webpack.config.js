973adfe77c2b737f853c2451eca8d14f3daf10a0
NK011 <work.nitish11@gmail.com>
2024-04-21 12:48:02 +0530

    Output management

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,24 @@
 const path = require("path");
-const toml = require("toml");
-const yaml = require("yamljs");
-const json5 = require("json5");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    entry: "./src/index.js",
+    // entry: "./src/index.js",
+    entry: {
+        index: "./src/index.js",
+        print: "./src/print.js",
+    },
+    plugins: [
+        // generates own index.html file
+        // replaces existing index.html file in /dist
+        new HtmlWebpackPlugin({
+            title: "Output Management"
+        })
+    ],
     output: {
         // filename: "main.js",
-        filename: "bundle.js",
+        // filename: "bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ["csv-loader"],
-            },
-            {
-                test: /\.xml$/i,
-                use: ["xml-loader"],
-            },
-            {
-                test: /\.toml$/i,
-                type: "json",
-                parser: {
-                    parse: toml.parse,
-                },
-            },
-            {
-                test: /\.yaml$/i,
-                type: "json",
-                parser: {
-                    parse: yaml.parse,
-                },
-            },
-            {
-                test: /\.json5$/i,
-                type: "json",
-                parser: {
-                    parse: json5.parse,
-                },
-            },
-        ],
-    },
-    resolve: {
-        alias: {
-            // import icon from '@assets/icon.svg';
-            "@assets": path.resolve(__dirname, "assets"),
-            
-            // /components
-            // |-- /c1
-            // |   |-- index.jsx
-            // |   |-- styles.css
-            // |   |-- assets
-            // |       |-- image1.jpg
-            // |       |-- image2.png
-            // |-- /c2
-            //     |-- index.jsx
-            //     |-- styles.css
-            //     |-- assets
-            //         |-- icon.svg
-            //         |-- background.jpg
-            '@c1-assets': path.resolve(__dirname, 'components', 'c1', 'assets'),
-        },
+        clean: true, // removes unused files frmo ./dist
     },
 };
