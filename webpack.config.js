0f64b4234293ca052c7040a7e01d61a0a12a7e8a
NK011 <work.nitish11@gmail.com>
2024-04-21 13:42:18 +0530

    Development module

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     // entry: "./src/index.js",
+    mode: "development",
     entry: {
         index: "./src/index.js",
         print: "./src/print.js",
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: "./dist" // This tells webpack-dev-server to serve the files from the dist directory on localhost:8080.
     },
     plugins: [
         // generates own index.html file
         // replaces existing index.html file in /dist
         new HtmlWebpackPlugin({
-            title: "Output Management"
+            title: "Development"
         })
     ],
     output: {
@@ -20,5 +25,9 @@ module.exports = {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
         clean: true, // removes unused files frmo ./dist
+        publicPath: "/" // makes the processed files available at the specified path
     },
+    optimization: {
+        runtimeChunk: "single" // The optimization.runtimeChunk: 'single' was added because in this example we have more than one entrypoint on a single HTML page
+    }
 };
